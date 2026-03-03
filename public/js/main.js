562adcb503c6bb572e7d506f5c04cc3ec63f35ce
joão soares <joaovitoralves200810@gmail.com>
2026-03-03 10:33:57 -0300

    Configurando Barra Lateral Esquerda e Adicionando Botão de Postagem

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+function abrirJanela() {
+  document.getElementById("overlay").style.display = "flex";
+}
+
+function fecharJanela() {
+  document.getElementById("overlay").style.display = "none";
+}
